cf684a8dc054fff87d2c3d81760707daf666804b
Nixon Lolila Ramin <nixonlr@gmail.com>
2014-12-31 08:05:34 -0500

    random starter implemented

--- src/scripts/components/board.jsx
+++ src/scripts/components/board.jsx
@@ -12,7 +12,15 @@ var Board = React.createClass({
 	startGame: function(event){
 		event.target.style.display = "none";
 		this.props.game.startGame();
+		if (this.pcStarts()){
+			var id = this.props.game.pcStartId();
+			React.renderComponent(<Mark mark="O"/>, document.getElementById(id));
+		}
 		this.started = true;
+	},
+
+	pcStarts: function(){
+		return Math.floor(Math.random() * 2) == 1;
 	},
 
 	handleClick: function(event){
